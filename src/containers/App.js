0c979bef8955962e24508007092af762ccdff64d
wcv007 <varunsingla2007@gmail.com>
2020-04-21 02:19:10 +0530

    first trial commit

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
+import ErrorBoundary from "../components/ErrorBoundary";
 import "./App.css";
 
 class App extends Component {
@@ -42,7 +43,9 @@ class App extends Component {
             searchfield={this.state.searchField}
           />
           <Scroll>
-            <CardList robots={filteredRobots} />
+            <ErrorBoundary>
+              <CardList robots={filteredRobots} />
+            </ErrorBoundary>
           </Scroll>
         </div>
       );
